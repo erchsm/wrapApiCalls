4b73c36add04e3b46c0a74423587c51e3dcf8001
Leran Firer <leranf@gmail.com>
2016-07-21 10:34:07 -0700

    call for creating collections

---
+++ createCollection.js
@@ -0,0 +1,21 @@
+var rp = require('request-promise');
+var API_KEY = require('./config').API_KEY;
+
+collectionCreationOptions = {
+  method: 'POST',
+  uri: 'https://api.wrap.co/api/card_collections',
+  headers: {
+    'Authorization': 'Bearer ' + API_KEY,
+    'Content-Type': 'application/json'
+  },
+  body: {
+    name: 'continueConversationPages'
+  },
+  json: true
+};
+
+rp(collectionCreationOptions)
+  .then(function(res) {
+    console.log(res);  
+  });
+
