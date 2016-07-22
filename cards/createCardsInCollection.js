8ec7a653563c4cb31a55574fa89ed1c98577ac0e
Leran Firer <leranf@gmail.com>
2016-07-22 13:24:54 -0700

    adds function to clone multiple cards into a collection by id

--- cards/createCardsInCollection.js
+++ cards/createCardsInCollection.js
@@ -1,34 +1,42 @@
 var rp = require('request-promise');
 var API_KEY = require('../config').API_KEY;
 
-var collection_id = 'b90b98f1-b84b-4766-bf37-24bb798bd454';
-var card_id = '9cce12d7-7329-4e8e-a564-4371d73aa7ef';
-var card_name = 'SAMSUNG GALAXY 6';
-var data_label = 'SAMSUNG GALAXY 6';
+var cards = [];
 
-var createOptions = {
-  method: 'POST',
-  uri: 'https://api.wrap.co/api/cards/' + card_id + '/clone',
-  headers: {
-    'Authorization': 'Bearer ' + API_KEY,
-    'Content-Type': 'application/json'
-  },
-  body: {
-    collection_id: collection_id,
-    data: {
-      card_name: card_name, 
-      data_label: data_label
-    },
-    mapping: {
-      card_name: card_name, 
-      'token label': data_label
-    },
-  },
-  json: true
-};
+cards.forEach(function(card, i) {
+  (function(i) {
+    var collection_id = '38ada967-5ea8-4703-a28d-c0fb4f236d68';
+    var card_id =  card.id;
+    var card_name = card.name;
+    var data_label = card.name;
+    
+    var createOptions = {
+      method: 'POST',
+      uri: 'https://api.wrap.co/api/cards/' + card_id + '/clone',
+      headers: {
+        'Authorization': 'Bearer ' + API_KEY,
+        'Content-Type': 'application/json'
+      },
+      body: {
+        collection_id: collection_id,
+        data: {
+          card_name: card_name, 
+          data_label: data_label
+        },
+        mapping: {
+          card_name: card_name, 
+          'token label': data_label
+        },
+      },
+      json: true
+    };
 
-rp(createOptions)
-  .then(function(res) {
-    console.log(res);  
-  });
+    rp(createOptions)
+      .then(function(res) {
+        console.log('created card' + i);  
+      });
+    
+  })(i);
+});
+
 
