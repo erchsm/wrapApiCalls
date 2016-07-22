144903564df003fc031ebe158260c0960ad4f7fa
Leran Firer <leranf@gmail.com>
2016-07-22 13:37:21 -0700

    creates angular services for getting cards in a collection, creating a new wrap, and sharing it over sms

---
+++ vmware/CardCollectionService.js
@@ -0,0 +1,23 @@
+angular.module('CardCollectionService', []).factory('CardCollection', ['$http', ($http) => {
+    
+  getCardsInCollection: () => {
+
+    const cardCollectionId = '38ada967-5ea8-4703-a28d-c0fb4f236d68';
+
+    const getOptions = {
+      method: 'GET',
+      uri: `https://api.wrap.co/api/cards/collections/search?card_collection_ids=${cardCollectionId}`,
+      headers: {
+        Authorization: `Bearer  ${process.env.API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    };
+
+    $http(getOptions)
+      .then(cards => cards)
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
+
+}]);
